de7b958386c64e03a07d91468bba8e4b91324e00
AndyPilozo <88100571+adpilozo@users.noreply.github.com>
2025-01-27 21:04:37 -0500

    Delete examenU2 directory

--- examenU2/src/main/java/org/espe/examenu2/repositories/VehiculoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.espe.examenu2.repositories;
-
-import org.espe.examenu2.model.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
-}
