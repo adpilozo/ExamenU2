6ea00f57aa5283d2367780b1725b33eeac4bc851
AndyPilozo <88100571+adpilozo@users.noreply.github.com>
2025-01-27 21:04:27 -0500

    Delete ExamenU2_1 directory

--- ExamenU2_1/src/main/java/org/espe/examenu2_1/repositories/ConductorRepository.java
+++
@@ -1,8 +0,0 @@
-package org.espe.examenu2_1.repositories;
-
-
-import org.espe.examenu2_1.model.Conductor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConductorRepository extends JpaRepository<Conductor, Long> {
-}
